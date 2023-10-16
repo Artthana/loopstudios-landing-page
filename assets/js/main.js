31e2fb949fca0c46a09cff7562ae9af106fa9604
Wongsathorn <>
2023-10-16 17:53:30 +0700

    update layout and add javascript

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+function open_menu(){
+    let menu = document.getElementById('menu-mb')
+    menu.classList.add('active')
+    console.log("test....")
+}
+
+function close_menu(){
+    let menu = document.getElementById('menu-mb')
+    menu.classList.remove('active')
+}
